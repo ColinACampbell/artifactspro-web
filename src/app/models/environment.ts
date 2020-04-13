b39003de0a379e75a37921496b712acee5e5bb0d
Colin Campbell <prontobol@gmail.com>
2020-04-12 23:05:28 -0500

    Added favicon

--- src/app/models/environment.ts
+++ src/app/models/environment.ts
@@ -5,6 +5,7 @@ export class Environment {
 
     constructor() {
         this.url = this.prod ? "https://artifactspro.herokuapp.com/" : 'http://localhost:3000/'; 
+        console.log(this.url)
     }
 
     public baseURL() : String
