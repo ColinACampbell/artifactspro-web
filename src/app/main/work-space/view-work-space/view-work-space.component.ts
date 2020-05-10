fd47667c17e454176d0653ba6144d48f7a121f1d
Colin Campbell <prontobol@gmail.com>
2020-05-09 20:06:44 -0500

    Added layout

--- src/app/main/work-space/view-work-space/view-work-space.component.ts
+++ src/app/main/work-space/view-work-space/view-work-space.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-work-space',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewWorkSpaceComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  public goBack()
+  {
+    this.router.navigate(['/app'])
   }
 
 }
