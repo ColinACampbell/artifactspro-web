2163a7411cb27c3797cce744c020b883034bad8c
Colin Campbell <prontobol@gmail.com>
2020-10-20 23:11:52 -0500

    Refractor artifacts

---
+++ src/app/main/dialogs/artifacts/delete-artifact-dialog/delete-artifact-dialog.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
+import { ArtifactsService } from 'src/app/services/artifacts.service';
+import { observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-delete-artifact-dialog',
+  templateUrl: './delete-artifact-dialog.component.html',
+  styleUrls: ['./delete-artifact-dialog.component.css']
+})
+export class DeleteArtifactDialogComponent implements OnInit {
+
+  constructor(
+    private dialogRef:MatDialogRef<DeleteArtifactDialogComponent>,
+    private artServ:ArtifactsService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public dialogData: any
+  ) { }
+
+  public isNameCorrect:Boolean = false;
+  private artifactName:String;
+  private artID: number;
+
+  ngOnInit() {
+    this.artifactName = this.dialogData.artifact_name;
+    this.artID = this.dialogData.art_id;
+  }
+
+  public onType(artifactName)
+  {
+    if (this.artifactName === artifactName)
+      this.isNameCorrect = true
+    else
+      this.isNameCorrect = false;
+  }
+
+  public deleteArtifact()
+  {
+    this.artServ.deleteArtifact(this.artID)
+    .subscribe((observable)=>{
+      const message = observable['message'];
+      if (message === 'done')
+      {
+        const snackBarRef = this.snackBar.open(`The Artifact '${this.artifactName}' was deleted`,'Okay');
+        snackBarRef.afterDismissed()
+        .subscribe((_)=>{
+          this.dialogRef.close();
+          this.router.navigate(['/app'])
+        })
+      } else
+      {
+        this.snackBar.open('Looks like something went wrong')
+      }
+    });
+  }
+
+  public closeDialog()
+  {
+    this.dialogRef.close()
+  }
+}
---
+++ src/app/main/dialogs/artifacts/upload-artifact-dialog/upload-dialog.component.ts
@@ -0,0 +1,125 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { ADocument } from 'src/app/models/adocument';
+import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
+import { ArtifactsService } from 'src/app/services/artifacts.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { DocumentService } from 'src/app/services/document.service';
+import { HttpEvent } from '@angular/common/http';
+
+interface DialogData {
+  name: String;
+  content: String;
+  dateCreatedString: String;
+}
+
+@Component({
+  selector: 'app-upload-dialog',
+  templateUrl: './upload-dialog.component.html',
+  styleUrls: ['./upload-dialog.component.css']
+})
+
+export class UploadDialogComponent implements OnInit {
+
+  public fileData : String | ArrayBuffer;
+  public uploadProgress : number;
+  public hasUploaded:boolean = false;
+
+  constructor(
+    public dialogRef: MatDialogRef<UploadDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public dialogData: any,
+    private snackBar: MatSnackBar,
+    private artServ: ArtifactsService,
+    private docServ: DocumentService,
+    private route : ActivatedRoute) { }
+ 
+  public selectedFile:File = null;
+
+  ngOnInit() {
+  }
+
+  public fileInputChange(event)
+  {
+    this.selectedFile = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.readAsDataURL(this.selectedFile);
+    reader.onload = () => {
+        this.fileData = reader.result;
+    };
+  }
+
+  closeDialog()
+  {
+    this.dialogRef.close();
+  }
+
+  removeFile()
+  {
+    this.selectedFile = null;
+  }
+
+  uploadFile(name:String,comment:String)
+  {
+    let artID = this.dialogData.art_id;
+    let lastModifiedDate = new Date(this.selectedFile.lastModified); // place the time stamp in the date object's contructor
+    let lastModified = this.formatDate(lastModifiedDate);
+    let uploadedDate = new Date();
+    let dateUploaded = this.formatDate(uploadedDate);
+
+    if (name.length === 0 || comment.length === 0)
+    {
+      this.snackBar.open("Input fields cannot be empty","Okay");
+      return;
+    }
+
+    let document = {}
+
+    document['version'] = name;
+    document['comment'] = comment;
+    document['date_modified'] = lastModified;
+    document['date_uploaded'] = dateUploaded;
+    document['data'] = this.fileData;
+    document['file_type'] = this.selectedFile.type
+
+    this.docServ.uploadDocument(artID,document)
+    .subscribe((event:HttpEvent<any>)=>{
+
+      let totalData = event['total'];
+      let dataLoaded = event['loaded'];
+
+      let message = event['statusText'];
+
+      this.hasUploaded = true;
+
+      if (message === 'OK')
+      {
+        this.docServ.getDocuments(this.dialogData.art_id)
+        let snackbarRef = this.snackBar.open('Document uploaded successfuly','Okay',
+        {
+          duration : 5000
+        });
+        
+        snackbarRef.onAction()
+        .subscribe((_)=>{
+          this.dialogRef.close();
+        })
+      }
+      this.uploadProgress = (dataLoaded / totalData) * 100;
+    })
+  }
+
+  private formatDate(date:Date) : String
+  {
+    let year = date.getFullYear();
+    let day = date.getDate() + ""
+    let month = (date.getMonth() + 1) + "";
+    // parse in full format dd/mm/yyyy instead of d/m/yyyy
+    if(day.length === 1)
+      day = "0"+day;
+    
+    if(month.length === 1)
+      month = "0"+month;
+    
+    return `${day}/${month}/${year}`;
+  }
+}
