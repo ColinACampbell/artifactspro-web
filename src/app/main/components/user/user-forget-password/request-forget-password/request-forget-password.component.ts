47acfd8ac23609afef809ad3ceb542f09db01e14
Colin Campbell <prontobol@gmail.com>
2021-04-14 22:19:47 -0500

    Set up password recovery

--- src/app/main/components/user/user-forget-password/request-forget-password/request-forget-password.component.ts
+++ src/app/main/components/user/user-forget-password/request-forget-password/request-forget-password.component.ts
@@ -15,20 +15,31 @@ export class RequestForgetPasswordComponent implements OnInit {
     private userService : UserService
   ) { }
 
+  public isProcessingRequest : boolean = false;
+  public isRequestSent : boolean = false;
+
   ngOnInit(): void {
   }
 
   // This method is un tested
   public requestPassworRecovery(email:string)
   {
+    this.isProcessingRequest = true;
     this.userService.requestPasswordRecovery(email)
     .subscribe((response : HttpResponse<Object>)=>{
+      this.isProcessingRequest = false;
+      
       const okayStatus = 200;
       const userNotFoundStatus = 404;
       if (response.status === okayStatus)
-        this.snackBar.open("An Email was sent, please check & follow the steps to recovery","Okay")
+        this.isRequestSent = true; // make the ui visible for validation
       else if (response.status === userNotFoundStatus) 
         this.snackBar.open("Sorry, but this account is not valid","Okay")
+    },(error)=>{
+      this.isProcessingRequest = false;
+      const badStatus = 500;
+      if (error['status'] === badStatus)
+        this.snackBar.open("Something went wrong, please try again","Okay")
     })
   }
 }
